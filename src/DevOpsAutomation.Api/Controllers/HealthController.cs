a275c327977335f8860fc2f653a4b1ecb1e35da6
naieramostafa <naieramostafa80@gmail.com>
2026-09-14 03:53:07 +0300

    Fix health endpoint info disclosure

--- src/DevOpsAutomation.Api/Controllers/HealthController.cs
+++ src/DevOpsAutomation.Api/Controllers/HealthController.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using DevOpsAutomation.Core.Configuration;
 
 namespace DevOpsAutomation.Api.Controllers;
 
@@ -9,24 +7,13 @@ namespace DevOpsAutomation.Api.Controllers;
 [Produces("application/json")]
 public class HealthController : ControllerBase
 {
-    private readonly IOptions<LlmSettings> _llmSettings;
-
-    public HealthController(IOptions<LlmSettings> llmSettings)
-    {
-        _llmSettings = llmSettings;
-    }
-
     [HttpGet]
     public ActionResult<object> Get()
     {
-        var settings = _llmSettings.Value;
         return Ok(new
         {
             status = "Healthy",
-            timestamp = DateTime.UtcNow,
-            llmProvider = settings.Provider,
-            llmConfigured = !string.IsNullOrWhiteSpace(settings.ApiKey),
-            version = typeof(HealthController).Assembly.GetName().Version?.ToString() ?? "unknown"
+            timestamp = DateTime.UtcNow
         });
     }
 }
